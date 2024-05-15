809fcc051e83f0852dfc246da17f9e1972430615
GabRodVal <lotorrpg@gmail.com>
2024-05-15 18:30:05 -0300

    add goblin animation

--- index.js
+++ index.js
@@ -1,3 +1,85 @@
+var hero = "Fulano"
+var heroName = document.getElementById('heroname');
+heroName.innerHTML = hero;
+
+var heroName2 = document.getElementById('heroname2')
+heroName2.innerHTML = hero
+
+function changeName(){
+    do{
+        var hero = window.prompt("Qual o nome do heroi?");
+        console.log(hero)
+    }while(hero == "")
+    heroName.innerHTML = hero;
+}
+
+//const goblinImg = new Image();
+//goblinImg.style.position = "absolute";
+//goblinImg.src = "img/goblin.png"
+//goblinImg.style.width = "20%"
+//goblinImg.style.transform = "translateY(-50%)";
+
+var screenwidth = document.body.clientWidth
+//goblinImg.style.left = screenwidth - 100 + "px";
+
+function spawnGoblin() {
+    let newGoblin =  document.createElement('img')
+    newGoblin.src = "img/goblin.png";
+    newGoblin.style.position = "absolute";
+    newGoblin.style.width = "20%";
+    newGoblin.style.transform = "translateY(-50%)";
+    newGoblin.style.left = screenwidth + "px";
+    document.getElementById("goblins").appendChild(newGoblin);
+
+    return newGoblin
+}
+
+function goblinStride() {
+    let goblinCamp = [];
+
+    function summonGoblin(){
+        if (goblinCamp.length <= 5 && document.hasFocus()){
+            goblinCamp.push(spawnGoblin());
+        }
+    }
+
+    summonGoblin();
+
+    function goblinAnimation() {
+        if (goblinCamp.length > 0){
+            for (f = 0; f<goblinCamp.length; f++){
+                let gPosition = parseInt(goblinCamp[f].style.left.slice(0, -2));
+                if(gPosition <= -goblinCamp[f].width){
+                    document.getElementById("goblins").removeChild(goblinCamp[f]);
+                    goblinCamp.shift();
+                } else {
+                    if (document.hasFocus()){
+                        goblinCamp[f].style.left = (gPosition - 5) + "px"
+                    }
+                }
+            }
+        }
+        //let gPosition = parseInt(goblinImg.style.left.slice(0, -2))
+        //if (gPosition <= -goblinImg.width){
+        //    document.getElementById("goblins").removeChild(goblinImg);
+        //    goblinCamp.shift();
+        //} else {
+        //    goblinImg.style.left = (gPosition - 5) + "px"
+        //}
+        //console.log(gPosition)
+    }
+
+    setInterval(goblinAnimation, 10);
+    setInterval(summonGoblin, 2000);
+}
+
+
+
+
+var experience = 0;
+var heroLevel = document.getElementById('herolevel');
+heroLevel.innerHTML = level(experience)
+
 function level(xp){
     if (xp <= 1000){
         return "Ferro"
@@ -19,7 +101,6 @@ function level(xp){
 }
 
 
+goblinStride()
 
-var hero = window.prompt("Qual o nome do heroi?", "Fulano");
-var experience = 0;
-console.log("O herói "+hero+" possui "+ exp + " e está no nível "+level(experience)+"!");
+console.log("O herói "+hero+" possui "+ experience + " e está no nível "+level(experience)+"!");
